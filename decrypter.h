4d1d455af9b7abc479b8107c275ea1e412c0d8f2
Rommeir <rommeir001022@gmail.com>
2025-06-19 18:30:59 +0300

    decrypter.h

--- decrypter.h
+++ decrypter.h
@@ -3,16 +3,13 @@
 
 #include "shared_data.h"
 
-typedef struct {
-    shared_data_t* shared;
-    int client_id;
-} decrypter_args_t;
+
 
 void* decrypter_thread(void* arg);
 void copy_encrypted_password(shared_data_t* shared, int* version_out, char** encrypted_out, int* len_out, int* key_len_out);
 void generate_random_key(char* key, int key_len);
 int is_printable_ascii(const char* str, unsigned int len);
 int handle_successful_decryption(shared_data_t* shared, int client_id, int version, const char* key, const char* encrypted, int encrypted_len, const char* output, unsigned int plain_len, unsigned long iter);
-void attempt_decryption_loop(shared_data_t* shared, int client_id, int version, const char* encrypted, int len, int key_len);
+void attempt_decryption_loop(shared_data_t* shared, int client_id, int version,  char* encrypted, int len, int key_len);
 
 #endif
